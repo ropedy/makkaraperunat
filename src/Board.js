d6e80fbf3adef84e25d8fc70211f421acad1ac34
ropedy <ropedy@gmail.com>
2020-04-29 13:28:39 +0300

    works

--- src/Board.js
+++ src/Board.js
@@ -16,20 +16,72 @@ const createCards = () => {
     });
   }
 
-  return cards;
+  return cards.map(c => [Math.random(), c]).sort().map(c => c[1]);
 }
 
 const Board = () => {
   const [ cards, setCards ] = useState(createCards());
+  const [ active, setActive ] = useState(true);
+  const [ completed, setCompleted ] = useState(false);
+  const [ tries, setTries ] = useState(0);
+
+  const resetGame = () => {
+    setCards(createCards());
+    setActive(true);
+    setCompleted(false);
+    setTries(0);
+  }
 
   const cardSelected = id => {
-    console.log(id);
+    const card = cards.find(c => c.id === id);
+    const previous = cards.find(c => c.flipped);
+
+    if (card.flipped || card.revealed || !active) {
+      return;
+    }
+
+    let newCards = cards.map(c => c.id !== id ? c : { ...c, flipped: true });
+
+    if (previous) {
+      const match = previous.letter === card.letter;
+
+      if (match) {
+        newCards = newCards.map(c => ![previous.id, card.id].includes(c.id) ? c : { ...c, revealed: true, flipped: false });
+      }
+      else {
+        setActive(false);
+
+        setTimeout(() => {
+          setCards(newCards.map(c => ({ ...c, flipped: false })));
+  
+          setActive(true);
+        }, 1000);
+      }
+
+      setTries(tries + 1);
+    }
+
+    setCards(newCards);
+
+    if (newCards.every(c => c.revealed)) {
+      setCompleted(true);
+
+      
+      setTimeout(() => {
+        resetGame();
+      }, 1000);
+    }
   }
   
+  const cls = `board${active ? ' active' : ''}${completed ? ' completed' : ''}`;
+
   return (
-    <div className='board'>
-      {cards.map(c => <Card {...c} cardSelected={cardSelected}></Card>)}
-    </div>
+    <>
+      <div className={cls}>
+        {cards.map(c => <Card key={c.id} {...c} cardSelected={cardSelected}></Card>)}
+      </div>
+      <div>Tries: {tries}</div>
+    </>
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -4,8 +4,10 @@ import './Card.css';
 const Card = props => {
   const showCard = props.flipped || props.revealed;
 
+  const cls = `card ${props.revealed ? ' revealed' : ''}`;
+
   return (
-    <div className='card' onClick={() => props.cardSelected(props.id)}>
+    <div className={cls} onClick={() => props.cardSelected(props.id)}>
       {showCard ? props.letter : '?'}
     </div>
   );
